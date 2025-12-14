c618bb51e44f4e255b46a367e2b7b71432802f9b
Anwar Hossain <anwar22038a@gmail.com>
2025-12-14 18:58:00 +0600

    updated blade view and pdf export

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 use App\Http\Controllers\SlipController;
 
-Route::get('/slip', [SlipController::class, 'show']);
+Route::get('/', [SlipController::class, 'show']);
 Route::get('/slip/pdf', [SlipController::class, 'downloadPdf'])->name('slip.pdf');
